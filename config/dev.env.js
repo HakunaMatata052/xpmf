de527b9f9b077f44e8ae7a3830cd2bc1f35621a0
Twinkleee <365189883@qq.com>
2018-11-27 09:40:09 +0800

    测试

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,7 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"https://localhost:44364/"'
+  API_ROOT: '"https://localhost:5001/"'
 })

2332438b5866c834090292922b52aaeb619d10ae
Eysa <48749526@qq.com>
2018-11-27 08:29:41 +0800

    1111111

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"//192.168.0.166:9001"'
+  API_ROOT: '"https://localhost:44364/"'
 })

64f2c33486417e9bd007568a11744ac08f18d494
Twinkleee <365189883@qq.com>
2018-11-27 09:53:19 +0800

    忽略文件

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,12 +2,6 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-
-
-
-
-
-
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_ROOT: '"https://localhost:5001/"'

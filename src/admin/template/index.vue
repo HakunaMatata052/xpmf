d203441e3ccedc903386770ea38bb49e8ae39aab
Eysa <48749526@qq.com>
2018-12-11 17:56:53 +0800

    修改模板价格提示 数额过大 的bug

--- src/admin/template/index.vue
+++ src/admin/template/index.vue
@@ -1,238 +1,383 @@
 <template>
-	<div id="template" v-loading="loading">
-		<el-card class="box-card" shadow="never">
-			<div slot="header" class="clearfix">
-				<span>模板列表</span>
-				<el-button type="primary" style="margin-left: 30px;" @click="editDialog('')">新建模板</el-button>
-			</div>
-			<el-row :gutter="20">
-				<el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="6" v-for="item in list" :key="item.id">
-					<div class="template-list">
-						<div class="img">
-							<img :src="item.fullpathThumbnail" alt="" />
-						</div>
-						<div class="info">
-							<h3>{{item.name}}</h3>
-							<p>编号 : {{item.code}}</p>
-							<div class="btn-group">
-								<el-button size="mini" type="primary" @click="editDialog(item.id)">编辑模板</el-button>
-								<el-button size="mini" type="danger" @click="del(item.id)">删除模板</el-button>
-							</div>
-						</div>
-						<div class="btn-group"></div>
-					</div>
-				</el-col>
-			</el-row>
-			<br>
-			<el-pagination background layout="total,prev, pager, next" :current-page.sync="page" :page-size="size" :total="total" @current-change="pageFn" v-if="total!=0">
-			</el-pagination>
-		</el-card>
-		<el-dialog title="模板内容" :visible.sync="dialogFormVisible" :fullscreen="false" @closed="close">
-			<el-form :model="form" :rules="rules" ref="form" v-loading="dialogloading">
-				<el-form-item label="模板名称" label-width="120px" prop="name">
-					<el-input v-model="form.name"></el-input>
-				</el-form-item>
-				<el-form-item label="案例地址" label-width="120px" prop="showcase">
-					<el-input v-model="form.showcase"></el-input>
-				</el-form-item>
-				<el-row :gutter="20">
-					<el-col :span="8">
-						<el-form-item label="缩略图" label-width="120px">
-							<el-upload class="img-uploader" name="upload" :action="$store.state.api+'template/picture'" :headers="headers" :show-file-list="false" :on-success="picSuccess">
-								<img v-if="form.fullpathPicture" :src="form.fullpathPicture" class="img">
-								<i v-else class="el-icon-plus img-uploader-icon"></i>
-							</el-upload>
-						</el-form-item>
-					</el-col>
-					<el-col :span="16">
-						<el-form-item label="模板分类" label-width="120px" prop="categoryId">
-							<el-select v-model="form.categoryId" placeholder="请选择模板类别">
-								<el-option :label="x.name" :value="x.id" v-for="x in categories" :key="x.id" v-loading="cateloading"></el-option>
-							</el-select>
-						</el-form-item>
-						<el-form-item label="模板编号" label-width="120px">
-							<el-input v-model="form.code"></el-input>
-						</el-form-item>
-						<el-form-item label="模板价格" label-width="120px" prop="price" :rules="[
+  <div
+    id="template"
+    v-loading="loading"
+  >
+    <el-card
+      class="box-card"
+      shadow="never"
+    >
+      <div
+        slot="header"
+        class="clearfix"
+      >
+        <span>模板列表</span>
+        <el-button
+          type="primary"
+          style="margin-left: 30px;"
+          @click="editDialog('')"
+        >新建模板</el-button>
+      </div>
+      <el-row :gutter="20">
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="8"
+          :lg="6"
+          :xl="6"
+          v-for="item in list"
+          :key="item.id"
+        >
+          <div class="template-list">
+            <div class="img">
+              <img
+                :src="item.fullpathThumbnail"
+                alt=""
+              />
+            </div>
+            <div class="info">
+              <h3>{{item.name}}</h3>
+              <p>编号 : {{item.code}}</p>
+              <div class="btn-group">
+                <el-button
+                  size="mini"
+                  type="primary"
+                  @click="editDialog(item.id)"
+                >编辑模板</el-button>
+                <el-button
+                  size="mini"
+                  type="danger"
+                  @click="del(item.id)"
+                >删除模板</el-button>
+              </div>
+            </div>
+            <div class="btn-group"></div>
+          </div>
+        </el-col>
+      </el-row>
+      <br>
+      <el-pagination
+        background
+        layout="total,prev, pager, next"
+        :current-page.sync="page"
+        :page-size="size"
+        :total="total"
+        @current-change="pageFn"
+        v-if="total!=0"
+      >
+      </el-pagination>
+    </el-card>
+    <el-dialog
+      title="模板内容"
+      :visible.sync="dialogFormVisible"
+      :fullscreen="false"
+      @closed="close"
+    >
+      <el-form
+        :model="form"
+        :rules="rules"
+        ref="form"
+        v-loading="dialogloading"
+      >
+        <el-form-item
+          label="模板名称"
+          label-width="120px"
+          prop="name"
+        >
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item
+          label="案例地址"
+          label-width="120px"
+          prop="showcase"
+        >
+          <el-input v-model="form.showcase"></el-input>
+        </el-form-item>
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <el-form-item
+              label="缩略图"
+              label-width="120px"
+            >
+              <el-upload
+                class="img-uploader"
+                name="upload"
+                :action="$store.state.api+'template/picture'"
+                :headers="headers"
+                :show-file-list="false"
+                :on-success="picSuccess"
+              >
+                <img
+                  v-if="form.fullpathPicture"
+                  :src="form.fullpathPicture"
+                  class="img"
+                >
+                <i
+                  v-else
+                  class="el-icon-plus img-uploader-icon"
+                ></i>
+              </el-upload>
+            </el-form-item>
+          </el-col>
+          <el-col :span="16">
+            <el-form-item
+              label="模板分类"
+              label-width="120px"
+              prop="categoryId"
+            >
+              <el-select
+                v-model="form.categoryId"
+                placeholder="请选择模板类别"
+              >
+                <el-option
+                  :label="x.name"
+                  :value="x.id"
+                  v-for="x in categories"
+                  :key="x.id"
+                  v-loading="cateloading"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item
+              label="模板编号"
+              label-width="120px"
+            >
+              <el-input v-model="form.code"></el-input>
+            </el-form-item>
+            <el-form-item
+              label="模板价格"
+              label-width="120px"
+              prop="price"
+              :rules="[
           { required: true, message: '价格不能为空' },
-          { pattern: /(^[1-9]([0-9]+)?(\.[0-9]{1,2})?$)|(^(0){1}$)|(^[0-9]\.[0-9]([0-9])?$)/, message: '请输入正确的金额' },
-          { max: 6, message: '金额过大' },
-        ]">
-							<el-input v-model="form.price"></el-input>
-						</el-form-item>
-					</el-col>
-				</el-row>
-				<el-form-item label="模板压缩包" label-width="120px" prop="source">
-					<el-input v-model="form.source" class="uploadInput"></el-input>
-					<el-upload class="upload" name="upload" :action="$store.state.api+'template/source/'" :headers="headers" :on-success="sourceSuccess" :show-file-list="false">
-						<el-button size="small" type="primary">点击上传</el-button>
-					</el-upload>
-					<i class="el-icon-success" v-if="sourceState"></i>
-				</el-form-item>
-				<el-form-item label="排序" label-width="120px">
-					<el-input v-model="form.ordering"></el-input>
-				</el-form-item>
-				<el-form-item label="摘要" label-width="120px">
-					<el-input v-model="form.description" type="textarea"></el-input>
-				</el-form-item>
-				<hr />
-				<el-form-item label="SEO标题" label-width="120px">
-					<el-input v-model="form.seoTitle"></el-input>
-				</el-form-item>
-				<el-form-item label="SEO关键词" label-width="120px">
-					<el-input v-model="form.seoKeywords"></el-input>
-				</el-form-item>
-				<el-form-item label="SEO描述" label-width="120px">
-					<el-input v-model="form.seoDescription" type="textarea"></el-input>
-				</el-form-item>
-			</el-form>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click="dialogFormVisible = false">取 消</el-button>
-				<el-button type="primary" @click="edit('form',form.id)">确 定</el-button>
-			</div>
-		</el-dialog>
-	</div>
+					 { type: 'number', message: '价格必须为数字值'}
+        ]"
+            >
+              <el-input
+                type="number"
+                v-model.number="form.price"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-form-item
+          label="模板压缩包"
+          label-width="120px"
+          prop="source"
+        >
+          <el-input
+            v-model="form.source"
+            class="uploadInput"
+          ></el-input>
+          <el-upload
+            class="upload"
+            name="upload"
+            :action="$store.state.api+'template/source/'"
+            :headers="headers"
+            :on-success="sourceSuccess"
+            :show-file-list="false"
+          >
+            <el-button
+              size="small"
+              type="primary"
+            >点击上传</el-button>
+          </el-upload>
+          <i
+            class="el-icon-success"
+            v-if="sourceState"
+          ></i>
+        </el-form-item>
+        <el-form-item
+          label="排序"
+          label-width="120px"
+        >
+          <el-input v-model="form.ordering"></el-input>
+        </el-form-item>
+        <el-form-item
+          label="摘要"
+          label-width="120px"
+        >
+          <el-input
+            v-model="form.description"
+            type="textarea"
+          ></el-input>
+        </el-form-item>
+        <hr />
+        <el-form-item
+          label="SEO标题"
+          label-width="120px"
+        >
+          <el-input v-model="form.seoTitle"></el-input>
+        </el-form-item>
+        <el-form-item
+          label="SEO关键词"
+          label-width="120px"
+        >
+          <el-input v-model="form.seoKeywords"></el-input>
+        </el-form-item>
+        <el-form-item
+          label="SEO描述"
+          label-width="120px"
+        >
+          <el-input
+            v-model="form.seoDescription"
+            type="textarea"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="edit('form',form.id)"
+        >确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			list: [],
-			page: 0,
-			size: 0,
-			total: 0,
-			dialogFormVisible: false,
-			headers: {},
-			form: {},
-			rules: {
-				name: [{
-					required: true,
-					message: '请输入名称',
-					trigger: 'blur'
-				},
-				{
-					min: 2,
-					max: 20,
-					message: '长度在 2 到 20 个字符',
-					trigger: 'blur'
-				}],
-				showcase: {
-					required: true,
-					message: '请输入案例地址（以http://开头）',
-					trigger: 'blur'
-				},
-				source: {
-					required: true,
-					message: '请上传模板',
-					trigger: 'blur'
-				},
+  data() {
+    return {
+      list: [],
+      page: 0,
+      size: 0,
+      total: 0,
+      dialogFormVisible: false,
+      headers: {},
+      form: {},
+      rules: {
+        name: [{
+          required: true,
+          message: '请输入名称',
+          trigger: 'blur'
+        },
+        {
+          min: 2,
+          max: 20,
+          message: '长度在 2 到 20 个字符',
+          trigger: 'blur'
+        }],
+        showcase: {
+          required: true,
+          message: '请输入案例地址（以http://开头）',
+          trigger: 'blur'
+        },
+        source: {
+          required: true,
+          message: '请上传模板',
+          trigger: 'blur'
+        },
 
-				categoryId: [{
-					required: true,
-					message: '请选择类别',
-					trigger: 'change'
-				}]
-			},
-			categories: [],
-			sourceState: false,
-			loading: true,
-			cateloading: true,
-			dialogloading: true
-		};
-	},
-	created() {
-		this.getList(1);
-		this.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
-	},
-	methods: {
-		getList(val) {
-			var that = this;
-			that.get_json(that.$store.state.api + 'template/page/' + val, function (data) {
-				that.list = data.data;
-				that.page = data.page;
-				that.size = data.size;
-				that.total = data.total;
-				that.loading = false;
-			})
-		},
-		pageFn(val) {
-			this.getList(val);
-			this.gotop();
-		},
-		editDialog(id) {
-			var that = this;
-			that.dialogFormVisible = true;
-			if (id.length != 0) {
-				that.get_json(that.$store.state.api + 'template/' + id, function (data) {
-					that.form = data;
-					that.dialogloading = false;
-				});
-			} else {
-				that.dialogloading = false;
-			}
-			that.get_json(that.$store.state.api + 'template/categories/', function (data) {
-				that.categories = data;
-				that.cateloading = false;
+        categoryId: [{
+          required: true,
+          message: '请选择类别',
+          trigger: 'change'
+        }]
+      },
+      categories: [],
+      sourceState: false,
+      loading: true,
+      cateloading: true,
+      dialogloading: true
+    };
+  },
+  created() {
+    this.getList(1);
+    this.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+  },
+  methods: {
+    getList(val) {
+      var that = this;
+      that.get_json(that.$store.state.api + 'template/page/' + val, function (data) {
+        that.list = data.data;
+        that.page = data.page;
+        that.size = data.size;
+        that.total = data.total;
+        that.loading = false;
+      })
+    },
+    pageFn(val) {
+      this.getList(val);
+      this.gotop();
+    },
+    editDialog(id) {
+      var that = this;
+      that.dialogFormVisible = true;
+      if (id.length != 0) {
+        that.get_json(that.$store.state.api + 'template/' + id, function (data) {
+          that.form = data;
+          that.dialogloading = false;
+        });
+      } else {
+        that.dialogloading = false;
+      }
+      that.get_json(that.$store.state.api + 'template/categories/', function (data) {
+        that.categories = data;
+        that.cateloading = false;
 
-			});
-		},
-		edit(formName, val) {
-			var that = this;
-			that.$refs[formName].validate((valid) => {
-				if (valid) {
-					if (val != undefined) {
-						that.put_json(that.$store.state.api + 'template/' + val, that.form, function (data) {
-							that.$message({
-								type: 'success',
-								message: '提交成功！'
-							});
-							that.getList(that.page);
-							that.dialogFormVisible = false;
-						})
-					} else {
-						that.post_json(that.$store.state.api + 'template/', that.form, function (data) {
-							that.$message({
-								type: 'success',
-								message: '提交成功！'
-							});
-							that.getList(that.page);
-							that.dialogFormVisible = false;
-						})
-					}
-				} else {
-					that.$message({
-						type: 'error',
-						message: '请填写完整内容！'
-					});
-					return false;
-				}
-			});
-		},
-		del(val) {
-			var that = this;
-			that.del_json(that.$store.state.api + 'template/' + val, function (data) {
-				that.$message({
-					type: 'success',
-					message: '删除成功!!'
-				});
-				that.getList(that.page);
-			})
-		},
-		close() {
-			this.form = {};
-			this.dialogFormVisible = false;
-		},
-		sourceSuccess(res) {
-			this.$set(this.form, 'source', res.fileName);
-			this.sourceState = true;
-			//this.form.source = res;
-		},
-		picSuccess(res, file) {
-			this.form.picture = res.fileName;
-			this.$set(this.form, 'fullpathPicture', URL.createObjectURL(file.raw));
-		}
-	}
+      });
+    },
+    edit(formName, val) {
+      var that = this;
+      that.$refs[formName].validate((valid) => {
+        if (valid) {
+          if (val != undefined) {
+            that.put_json(that.$store.state.api + 'template/' + val, that.form, function (data) {
+              that.$message({
+                type: 'success',
+                message: '提交成功！'
+              });
+              that.getList(that.page);
+              that.dialogFormVisible = false;
+            })
+          } else {
+            that.post_json(that.$store.state.api + 'template/', that.form, function (data) {
+              that.$message({
+                type: 'success',
+                message: '提交成功！'
+              });
+              that.getList(that.page);
+              that.dialogFormVisible = false;
+            })
+          }
+        } else {
+          that.$message({
+            type: 'error',
+            message: '请填写完整内容！'
+          });
+          return false;
+        }
+      });
+    },
+    del(val) {
+      var that = this;
+      that.del_json(that.$store.state.api + 'template/' + val, function (data) {
+        that.$message({
+          type: 'success',
+          message: '删除成功!!'
+        });
+        that.getList(that.page);
+      })
+    },
+    close() {
+      this.form = {};
+      this.dialogFormVisible = false;
+    },
+    sourceSuccess(res) {
+      this.$set(this.form, 'source', res.fileName);
+      this.sourceState = true;
+      //this.form.source = res;
+    },
+    picSuccess(res, file) {
+      this.form.picture = res.fileName;
+      this.$set(this.form, 'fullpathPicture', URL.createObjectURL(file.raw));
+    }
+  }
 }
 </script>
 

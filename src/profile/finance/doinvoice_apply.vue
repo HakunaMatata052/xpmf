bc0d94de07e180fc9fb271136ed123b1693663e7
Eysa <48749526@qq.com>
2018-12-17 09:28:41 +0800

    修改输入字符串为空时弹窗提示语

--- src/profile/finance/doinvoice_apply.vue
+++ src/profile/finance/doinvoice_apply.vue
@@ -1,93 +1,146 @@
 <template>
-	<div id="record">
-		<el-alert title="购买 60 天内的金额可以在此页面申请开票，如单次开票金额未满 800 元，需要支付 25 元的快递费。" type="warning">
-		</el-alert>
-		<el-table :data="list" stripe @selection-change="handleSelectionChange" style="width: 100%">
-			<el-table-column type="selection" width="55">
-			</el-table-column>
-			<el-table-column prop="typeString" label="类型" width="80">
-			</el-table-column>
-			<el-table-column prop="amount" label="金额">
-				<template slot-scope="scope">
-					<el-button type="success" round size="mini" v-if="scope.row.type==20">{{scope.row.amount}}</el-button>
-					<el-button type="danger" round size="mini" v-else-if="scope.row.type==10">{{scope.row.amount}}</el-button>
-				</template>
-			</el-table-column>			
-			<el-table-column prop="creatime" label="操作时间">
-			</el-table-column>
-			<el-table-column prop="creatime" label="操作"  width="200">
-				<template slot-scope="scope">
-						<el-button size="small" @click="applyFn(scope.row.id)" type="primary">申请开票</el-button>
-				</template>
-			</el-table-column>
-		</el-table>
-		<br />
-		<el-pagination background layout="total,prev, pager, next" :current-page.sync="page" :page-size="size" :total="total" @current-change="pageFn" v-if="total!=0">
-		</el-pagination>
-		<br />
-		<el-button type="primary" @click="submit">申请开票</el-button>
-	</div>
+  <div id="record">
+    <el-alert
+      title="购买 60 天内的金额可以在此页面申请开票，如单次开票金额未满 800 元，需要支付 25 元的快递费。"
+      type="warning"
+    >
+    </el-alert>
+    <el-table
+      :data="list"
+      stripe
+      @selection-change="handleSelectionChange"
+      style="width: 100%"
+    >
+      <el-table-column
+        type="selection"
+        width="55"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="typeString"
+        label="类型"
+        width="80"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="amount"
+        label="金额"
+      >
+        <template slot-scope="scope">
+          <el-button
+            type="success"
+            round
+            size="mini"
+            v-if="scope.row.type==20"
+          >{{scope.row.amount}}</el-button>
+          <el-button
+            type="danger"
+            round
+            size="mini"
+            v-else-if="scope.row.type==10"
+          >{{scope.row.amount}}</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="creatime"
+        label="操作时间"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="creatime"
+        label="操作"
+        width="200"
+      >
+        <template slot-scope="scope">
+          <el-button
+            size="small"
+            @click="applyFn(scope.row.id)"
+            type="primary"
+          >申请开票</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <br />
+    <el-pagination
+      background
+      layout="total,prev, pager, next"
+      :current-page.sync="page"
+      :page-size="size"
+      :total="total"
+      @current-change="pageFn"
+      v-if="total!=0"
+    >
+    </el-pagination>
+    <br />
+    <el-button
+      type="primary"
+      @click="submit"
+    >申请开票</el-button>
+  </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				list: [],
-				page: 0,
-				size: 0,
-				total: 0,
-				applyId:[],
-			};
-		},
-		created() {
-			this.getList(1);
-		},
-		methods: {
-			getList(val) {
-				var that = this;
-				that.get_json(that.$store.state.api + 'invoice/apply/page/' + val, function(data) {
-					that.list = data.data;
-					that.page = data.page;
-					that.size = data.size;
-					that.total = data.total;
-				})
-			},
-			pageFn(val) {
-				this.getList(val);
-				this.gotop();
-			},
-			handleSelectionChange(val) {
-				this.applyId = [];
-				for(var i=0;i<val.length;i++){
-					this.applyId.push(val[i].id)
-				}
-				console.log(this.applyId)
-			},
-			applyFn(id){
-				var that =this;
-				that.put_json(that.$store.state.api + 'invoice/'+id+'/applying',{}, function(data) {
-					that.$message({
-						type: 'success',
-						message: '提交成功！'
-					});
-					that.getList(0)
-				})
-			},
-			submit() {
-				var that =this;
-				that.put_json(that.$store.state.api + 'invoice/'+that.applyId+'/applying',{}, function(data) {
-					that.$message({
-						type: 'success',
-						message: '提交成功！'
-					});
-					that.getList(0)
-				})
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      list: [],
+      page: 0,
+      size: 0,
+      total: 0,
+      applyId: [],
+    };
+  },
+  created() {
+    this.getList(1);
+  },
+  methods: {
+    getList(val) {
+      var that = this;
+      that.get_json(that.$store.state.api + 'invoice/apply/page/' + val, function (data) {
+        that.list = data.data;
+        that.page = data.page;
+        that.size = data.size;
+        that.total = data.total;
+      })
+    },
+    pageFn(val) {
+      this.getList(val);
+      this.gotop();
+    },
+    handleSelectionChange(val) {
+      this.applyId = [];
+      for (var i = 0; i < val.length; i++) {
+        this.applyId.push(val[i].id)
+      }
+      console.log(this.applyId)
+    },
+    applyFn(id) {
+      var that = this;
+      that.put_json(that.$store.state.api + 'invoice/' + id + '/applying', {}, function (data) {
+        that.$message({
+          type: 'success',
+          message: '提交成功！'
+        });
+        that.getList(0)
+      })
+    },
+    submit() {
+      var that = this;
+      if (that.applyId.length <= 0) {
+        that.$message({ type: "error", message: "请选择需开票的记录" });
+        return;
+      }
+      that.put_json(that.$store.state.api + 'invoice/' + that.applyId + '/applying', {}, function (data) {
+        that.$message({
+          type: 'success',
+          message: '提交成功！'
+        });
+        that.getList(0)
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
-
 </style>
--- src/profile/home/business.vue
+++ src/profile/home/business.vue
@@ -1,99 +1,154 @@
 <template>
-	<div id="template">
-		<el-table :data="list" stripe style="width: 100%">
-			<el-table-column prop="domain" label="绑定域名" width="260" show-overflow-tooltip>
-				<template slot-scope="scope">
-					<span v-if="scope.row.domain.length==0">暂未授权</span>
-					{{scope.row.domain}}
-				</template>
-			</el-table-column>
-			<el-table-column prop="authorizeDate" label="购买时间" width="200" show-overflow-tooltip>
-			</el-table-column>
-			<el-table-column label="操作" min-width="100">
-				<template slot-scope="scope">
-					<el-button size="small" @click="dialogBind(scope.row.id)" type="success" v-if="!scope.row.currentUsed">绑定</el-button>
-					<el-tag type="success" v-else>已绑定</el-tag>
-				</template>
-			</el-table-column>
-		</el-table>
-		<br  v-if="total!=0"/>
-		<el-pagination background layout="total,prev, pager, next" :current-page.sync="page" :page-size="size" :total="total" @current-change="pageFn" v-if="total!=0">
-		</el-pagination>
-		<br />
-		<el-button type="primary" @click="jump_href('/permission','_blank')">去购买</el-button>
-		<el-dialog title="绑定域名" :visible.sync="dialogBindDomain">
-			<el-alert title="授权只能绑定一次，请谨慎使用！" type="warning" show-icon>
-			</el-alert>
-			<br />
-			<el-row :gutter="20">
-				<el-col :span="21">
-					<el-input placeholder="请输入域名" v-model="domainForm.domain">
-						<template slot="prepend">Http://</template>
-					</el-input>
-				</el-col>
-				<el-col :span="3">
-					<el-button type="success" icon="el-icon-check" style="width: 100%;" @click="bindDomainFn"></el-button>
-				</el-col>
-			</el-row>
-		</el-dialog>
+  <div id="template">
+    <el-table
+      :data="list"
+      stripe
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="domain"
+        label="绑定域名"
+        width="260"
+        show-overflow-tooltip
+      >
+        <template slot-scope="scope">
+          <span v-if="scope.row.domain.length==0">暂未授权</span>
+          {{scope.row.domain}}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="authorizeDate"
+        label="购买时间"
+        width="200"
+        show-overflow-tooltip
+      >
+      </el-table-column>
+      <el-table-column
+        label="操作"
+        min-width="100"
+      >
+        <template slot-scope="scope">
+          <el-button
+            size="small"
+            @click="dialogBind(scope.row.id)"
+            type="success"
+            v-if="!scope.row.currentUsed"
+          >绑定</el-button>
+          <el-tag
+            type="success"
+            v-else
+          >已绑定</el-tag>
+        </template>
+      </el-table-column>
+    </el-table>
+    <br v-if="total!=0" />
+    <el-pagination
+      background
+      layout="total,prev, pager, next"
+      :current-page.sync="page"
+      :page-size="size"
+      :total="total"
+      @current-change="pageFn"
+      v-if="total!=0"
+    >
+    </el-pagination>
+    <br />
+    <el-button
+      type="primary"
+      @click="jump_href('/permission','_blank')"
+    >去购买</el-button>
+    <el-dialog
+      title="绑定域名"
+      :visible.sync="dialogBindDomain"
+    >
+      <el-alert
+        title="授权只能绑定一次，请谨慎使用！"
+        type="warning"
+        show-icon
+      >
+      </el-alert>
+      <br />
+      <el-row :gutter="20">
+        <el-col :span="21">
+          <el-input
+            placeholder="请输入域名"
+            v-model="domainForm.domain"
+          >
+            <template slot="prepend">Http://</template>
+          </el-input>
+        </el-col>
+        <el-col :span="3">
+          <el-button
+            type="success"
+            icon="el-icon-check"
+            style="width: 100%;"
+            @click="bindDomainFn"
+          ></el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
 
-	</div>
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			bind: false,
-			apiUrl: 'UserPermission/page/',
-			list: [],
-			page: 0,
-			size: 0,
-			total: 0,
-			dialogBindDomain: false,
-			domainForm: {
-				domain: '',
-				id: {}
-			}
-		};
-	},
-	created() {
-		if (this.$route.params.id) {
-			this.bind = true;
-			this.apiUrl = 'UserPermission/unused/site/'+this.$route.params.id+'/page/'
-		}
-		this.getList(1)
-	},
-	methods: {
-		getList(val) {
-			var that = this;
-			that.get_json(that.$store.state.api + that.apiUrl + val, function (data) {
-				that.list = data.data;
-				that.page = data.page;
-				that.size = data.size;
-				that.total = data.total;
-			})
-		},
-		pageFn(val) {
-			this.getList(val);
-			this.gotop();
-		},
-		dialogBind(id) {
-			this.dialogBindDomain = true;
-			this.domainForm.id = id;
-		},
-		bindDomainFn() {
-			var that = this;
-			that.put_json(that.$store.state.api + 'UserPermission/' + that.domainForm.id + '/bind/' + that.domainForm.domain, {}, function (data) {
-				that.$message({
-					type: 'success',
-					message: '绑定成功！'
-				});
-				that.getList(that.page);
-				that.dialogBindDomain = false;
-			})
-		}
-	}
+  data() {
+    return {
+      bind: false,
+      apiUrl: 'UserPermission/page/',
+      list: [],
+      page: 0,
+      size: 0,
+      total: 0,
+      dialogBindDomain: false,
+      domainForm: {
+        domain: '',
+        id: {}
+      }
+    };
+  },
+  created() {
+    if (this.$route.params.id) {
+      this.bind = true;
+      this.apiUrl = 'UserPermission/unused/site/' + this.$route.params.id + '/page/'
+    }
+    this.getList(1)
+  },
+  methods: {
+    getList(val) {
+      var that = this;
+      that.get_json(that.$store.state.api + that.apiUrl + val, function (data) {
+        that.list = data.data;
+        that.page = data.page;
+        that.size = data.size;
+        that.total = data.total;
+      })
+    },
+    pageFn(val) {
+      this.getList(val);
+      this.gotop();
+    },
+    dialogBind(id) {
+      this.dialogBindDomain = true;
+      this.domainForm.id = id;
+    },
+    bindDomainFn() {
+      var that = this;
+      if (that.domainForm.domain == "" || that.domainForm.domain.trim().length <= 0 || that.domainForm.domain == undefined) {
+        that.$message({ type: "error", message: "域名不能为空" });
+        return;
+      }
+      that.put_json(that.$store.state.api + 'UserPermission/' + that.domainForm.id + '/bind/' + that.domainForm.domain, {}, function (data) {
+        that.$message({
+          type: 'success',
+          message: '绑定成功！'
+        });
+        that.getList(that.page);
+        that.dialogBindDomain = false;
+      })
+    }
+  }
 }
 </script>
 
